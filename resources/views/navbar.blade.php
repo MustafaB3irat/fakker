4a89ae33d87e5bb96dad42ccd46940e998bd1a53
MustafaB3irat <mustafaadwi@gmail.com>
2019-12-17 21:11:39 +0200

    first commit initialized the project and designed main layout

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"> <img class="logo" src="{{asset('img/favicon.jpg')}}" height="40"> {{config('app.name' , 'فكر بغيرك')}}</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbar1">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="btn ml-2 btn-warning" href="#">Login</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+
+@section('content')
+    
+    <img src="{{asset('img/cover_fakker.jpg')}}" alt="cover photo" class="img-fluid" style=" position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);">
+
+ @endsection
